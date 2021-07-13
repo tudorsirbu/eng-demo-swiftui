f0a4f839bbe4c500f8900567069dd4f8938f3b9d
Tudor Sirbu <tudor@tutorful.co.uk>
2021-07-13 23:02:02 +0100

    Initial Commit

---
+++ Shared/EngDemoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EngDemoSwiftUIApp.swift
+//  Shared
+//
+//  Created by Tudor Sirbu on 13/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct EngDemoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
