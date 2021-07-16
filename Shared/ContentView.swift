976932a9b848756fc230533e37965b742aeec9fd
Tudor Sirbu <tudor@tutorful.co.uk>
2021-07-16 13:56:50 +0100

    Suggested solution

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,13 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
-    var booking: Booking
+    @State var booking: Booking
     
     var body: some View {
         VStack {
             
+            HStack {
+                Image("StudentAvatar")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+                    .frame(maxWidth: 80)
+                
+                Text(booking.student.firstName)
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .padding()
+            }
+            
+            Text(booking.subject)
+                .font(.callout)
+                .foregroundColor(Color.white)
+                .padding()
+            
+            HStack {
+                Text(booking.startTime, style: .date)
+                    .foregroundColor(.white)
+                    .padding()
+                
+                Text(booking.startTime, style: .time)
+                    .foregroundColor(.white)
+                    .padding()
+                
+                Text("£\(booking.price)")
+                    .foregroundColor(.white)
+            }
+            
             Button(action: {
                 // todo increase booking price by £10 with each tap
+                self.booking.price += 10
             }, label: {
                 Image(systemName: "sterlingsign.circle")
                 Text("Charge \(booking.student.firstName) more")
